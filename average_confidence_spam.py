c4c964581aa8942abe9a8baa76d4858bf049610d
HuitingZhengAvery <avery.zheng.ht@gmail.com>
2019-02-12 14:19:58 +1100

    Update average_confidence_spam.py

--- average_confidence_spam.py
+++ average_confidence_spam.py
@@ -1,3 +1,4 @@
+#this code calculates the average spam confidence in a file
 fname=input('enter a file name: ')
 xfile=open(fname)
 count=0
@@ -5,9 +6,9 @@ total=0
 total=float(total)
 count=float(count)
 for line in xfile:
-    if line.startswith('X-DSPAM-Confidence:'):
-        count=count+1
-        single=len(line[19:])
+    if line.startswith('X-DSPAM-Confidence:'): #extracting the value of spam confidences
+        count=count+1 #adding up the quantity of values
+        single=len(line[19:]) #exact position of wanted number
         single=float(single)
         total=total+single
 ave=total/count
